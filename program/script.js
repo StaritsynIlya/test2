fda2012db0c77776e9e773c5ff78315738771da1
StaritsynIlya <vladokc33@gmail.com>
2022-11-26 17:04:19 +0300

    приступаю к frontend разработке

--- program/script.js
+++ program/script.js
@@ -1,6 +1,8 @@
-var app = new Vue ({
-    el: '#app',
-    data: {
-        message: 'Hello, World'
+Vue.component('comments', {
+    template: '<input>',
+    data() {
+        return {
+            name: null
+        }
     }
 })
